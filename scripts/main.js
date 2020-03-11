2c185741f0719774c623eea5ddbec9f5c76d5ace
gallerdude <gallerdude@gmail.com>
2020-03-11 11:48:12 -0500

    Transforms and Main

--- scripts/main.js
+++ scripts/main.js
@@ -8,9 +8,13 @@ function CalculateCompoundTransform(transforms) {
     // if only one transform, set compound transform eequal to it
     // otherwise multiply all matrices together (in proper order)
     // `compound_transform = Matrix.multiply(...)`
-    var tranform_matrices = [];
+    var transform_matrices = transforms.map(transform => transform.mat4x4);
 
     compound_transform = new Matrix(4, 4); // change / remove this
+
+    transform_matrices.forEach(transform_matrix => {
+        compound_transform = Matrix.multiply([compound_transform, transform_matrix]);
+    });
 
     return compound_transform;
 }
@@ -20,7 +24,7 @@ function CalculateTransformedVertex(vertex) {
     // multiple vertex by compound_transform
     // `final_vertex = Matrix.multiply(...)`
     var final_vertex = new Vector(4); // change / remove this
-
+    final_vertex = vertex.dot(new Vector(compound_transform));
     return final_vertex;
 }
 
